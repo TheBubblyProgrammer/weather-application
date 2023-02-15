37d1cb3c7834e642d5db83ded2514d2d1a7c4a27
Anastacia <102198747+TheBubblyProgrammer@users.noreply.github.com>
2023-02-15 15:50:56 +0200

    added live data to the elements

--- script.js
+++ script.js
@@ -26,15 +26,57 @@ function showWeatherConditions(response) {
   let descriptionElement = document.querySelector(`#description`);
   let humidityElement = document.querySelector(`#humidity`);
   let windElement = document.querySelector(`#wind` );
+ let realFeelElement = document.querySelector("#realFeels")
+  let sunsetElement = document.querySelector("#Sunset");
+ let precipitationElement = document.querySelector(`#precipitation`);
   let iconElement = document.querySelector(`#icon`);
  
+
+
+
+  console.log(response.data.sys.sunset)
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  iconElement.setAttribute(`src`,`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png` );
+  realFeelElement.innerHTML = Math.round(response.data.main.feels_like);
+  sunsetElement.innerHTML =Unix(response.data.sys.sunset);
+precipitationElement.innerHTML = updatedPrecipitation(response.data);
+iconElement.setAttribute(`src`,`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png` );
 iconElement.setAttribute(`alt`, response.data.weather[0].description);
+}
+
+function Unix(unixTime) {
+  let sunsetDate = new Date(unixTime * 1000);
+  let sunsetHoursMin = sunsetDate.toLocaleTimeString("en-ZA", {
+  hour: `2-digit`,
+  minute: `2-digit`,
+});
+
+ let sunset = `${sunsetHoursMin}`
+  return sunset;
+}
+
+
+function updatedPrecipitation(response) {
+  if (response.hasOwnProperty("rain")) {
+    return `${Math.round(response.rain["1h"])}mm`;
+  } else {
+    return `0mm`;
+  }
+}
+
+function searchCity(city) {
+  let apiKey = `5ef4de8cd6b7fefcd7c42f98cf464ce8`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeatherConditions);
+}
+
+function submitForm(event) {
+  event.preventDefault();
+  let city = document.querySelector(`#search-city`).value;
+  searchCity(city);
 }
 
 
@@ -42,8 +84,11 @@ iconElement.setAttribute(`alt`, response.data.weather[0].description);
 
 
 
-
-let city = `hazyview`;
 let apiKey = `5ef4de8cd6b7fefcd7c42f98cf464ce8`;
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(showWeatherConditions);
+
+let searchForm = document.querySelector(`#search-form`);
+searchForm.addEventListener(`submit`, submitForm);
+//add default search queryy
+searchCity(`Nelspruit`);
